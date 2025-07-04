bc170aeecf828dc435f4695cc2f949f25b29bf77
brentjlaf <brentjlaf@hotmail.com>
2025-07-03 19:57:24 -0600

    Add icons to active effects

--- scripts.js
+++ scripts.js
@@ -1684,6 +1684,7 @@ function startTimedEffect(item, effectType, value, duration) {
     gameState.activeEffects.push({
         id: effectId,
         itemName: item.name,
+        icon: item.icon,
         effectType,
         value,
         expiresAt,
@@ -1738,7 +1739,14 @@ function renderEffectTimers() {
         }
         const div = document.createElement('div');
         div.className = 'effect-timer';
-        div.textContent = `${effect.itemName} ${Math.ceil(remaining / 1000)}s`;
+
+        const iconEl = document.createElement('img');
+        iconEl.className = 'effect-icon';
+        if (effect.icon) iconEl.dataset.icon = effect.icon;
+        iconEl.alt = effect.itemName;
+        div.appendChild(iconEl);
+
+        div.appendChild(document.createTextNode(` ${effect.itemName} ${Math.ceil(remaining / 1000)}s`));
         container.appendChild(div);
     });
 }
